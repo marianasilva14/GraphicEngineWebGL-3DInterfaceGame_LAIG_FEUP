378e1f21f517c4d02bac188bc043356d99171ff7
up201506197 <up201506197@fe.up.pt>
2017-10-03 16:32:18 +0100

    Added primitives and update MyGraphLeaf

--- MyGraphLeaf.js
+++ MyGraphLeaf.js
@@ -4,5 +4,25 @@
 **/
 
 function MyGraphLeaf(graph, xmlelem) {
+  this.part = null;
+  var type = graph.reader.getItem(xmlelem, 'type', ['rectangle', 'cylinder', 'sphere', 'triangle']);
+  var args = graph.reader.getString(xmlelem, 'args');
+
+  if(type == 'rectangle'){
+    return;
+  }
+  if(type == 'cylinder'){
+    return;
+  }
+  if(type == 'triangle'){
+    return;
+  }
+  if(type == 'sphere'){
+  }
+  else{
+  }
 }
 
+MyGraphLeaf.prototype.display = function () {
+  this.part.display();
+};
